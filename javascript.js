64f864c221a5162d262c30f59ae721a924bb9297
wgcuster3 <wgcuster3@gmail.com>
2026-03-18 20:02:37 -0400

    Add 'hover' effect to change the background color

--- javascript.js
+++ javascript.js
@@ -1,21 +1,19 @@
 function fillRow(row, gridSize){
     for (let i = 0; i < gridSize; i++){
-        //console.log(`Create item ${i}`);
         let gridItem = document.createElement("div")
-        gridItem.textContent = i;
-        //gridItem.style.margin = "2px";
-        //gridItem.style.border = "thin solid #000";
+        gridItem.style.border = "thin solid #a6a2a3";
         gridItem.style.flexGrow = "1";
+        gridItem.style.minHeight = "20px";
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.style.backgroundColor = "#d1065e";
+        });
         row.appendChild(gridItem);
     }
 }
 
 function createRow (gridSize) {
     for (let i = 0; i < gridSize; i++){
-        //console.log(`Create row ${i}`);
         let row = document.createElement("div");
-        //row.style.border = "thin solid #d1065e";
-        //row.style.margin = "10px";
         row.style.display = "flex";
         row.style.justifyContent = "space-between";
 
